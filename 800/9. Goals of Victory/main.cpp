8b0061c78295352bfd56cc2a99443459b8b6c673
Sahil Madaan <madaan.sahil27@gmail.com>
2025-12-26 17:48:57 +0530

    Goals of victory

---
+++ 800/9. Goals of Victory/main.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/problemset/problem/1877/A
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void solve(){
+    int n; cin>>n;
+    vector<int> a(n-1);
+    int ans = 0;
+    for(int i=0; i<n-1; i++){
+        cin>>a[i];
+        ans += a[i];
+    }
+    cout<<-ans<<endl;
+    return;
+}
+ 
+int main(){
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+}
