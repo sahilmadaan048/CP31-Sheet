837381f1bb33fca56b8631a8bb851483dc33d6ed
Sahil Madaan <madaan.sahil27@gmail.com>
2025-12-26 17:45:05 +0530

    How much does daytona cost

---
+++ 800/8. How Much Does Daytona Cost/main.cpp
@@ -0,0 +1,27 @@
+// https://codeforces.com/problemset/problem/1878/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vector<int> a(n);
+
+    bool flag = false;
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+        if(a[i] == k) {
+            flag = true;
+        }
+    }
+
+    if(flag) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        solve();
+    }
+}
