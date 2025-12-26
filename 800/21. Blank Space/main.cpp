6da1c308885116ba23b4f754922f70f071322681
Sahil Madaan <madaan.sahil27@gmail.com>
2025-12-26 18:54:23 +0530

    Blank space

---
+++ 800/21. Blank Space/main.cpp
@@ -0,0 +1,35 @@
+// https://codeforces.com/problemset/problem/1829/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+
+    int cnt = 0;
+    int maxi = 0;
+    for(int i=0; i<n; i++) {
+        if(a[i] == 0) {
+            cnt++;
+        }
+        else {
+            maxi = max(maxi, cnt);
+            cnt = 0;
+        }
+    }
+    maxi = max(maxi, cnt);
+    cout << maxi << "\n";
+    return;
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        solve();
+    }
+}
