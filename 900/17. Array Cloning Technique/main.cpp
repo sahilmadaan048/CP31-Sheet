e4576ff49d1eaab083f3c4eae23ed16e3c6a8ea5
Sahil Madaan <madaan.sahil27@gmail.com>
2026-01-01 18:41:36 +0530

    Array cloning tech

---
+++ 900/17. Array Cloning Technique/main.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/problemset/problem/1665/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++) cin >> a[i];
+    
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        solve();
+    }
+} 
