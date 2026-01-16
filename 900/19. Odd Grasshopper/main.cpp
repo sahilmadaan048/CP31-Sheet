146c724254b0432215d292caefde730821193468
Sahil Madaan <madaan.sahil27@gmail.com>
2026-01-17 02:33:59 +0530

    fixed

--- 900/19. Odd Grasshopper/main.cpp
+++ 900/19. Odd Grasshopper/main.cpp
@@ -1,5 +1,6 @@
 // https://codeforces.com/problemset/problem/1607/B
 
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,30 +8,63 @@ void solve() {
     long long x, n;
     cin >> x >> n;
 
-    long long r = n % 4;
+    long long D;
 
-    if (r == 1) {
-        if (x & 1) x += n;
-        else x -= n;
-    }
-    else if (r == 2) {
-        if (x & 1) x -= 1;
-        else x += 1;
-    }
-    else if (r == 3) {
-        if (x & 1) x -= (n - 1);
-        else x += (n - 1);
-    }
-    // r == 0 → no change
+    if (n % 4 == 1)
+        D = -n;
+    else if (n % 4 == 2)
+        D = 1;
+    else if (n % 4 == 3)
+        D = n + 1;
+    else
+        D = 0;
 
-    cout << x << "\n";
+    if (x % 2 == 0)
+        cout << x + D << "\n";
+    else
+        cout << x - D << "\n";
 }
 
 int main() {
     int t;
     cin >> t;
-    while (t--) solve();
+    while (t--) {
+        solve();
+    }
+    return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve() {
+//     long long x, n;
+//     cin >> x >> n;
+
+//     long long r = n % 4;
+
+//     if (r == 1) {
+//         if (x & 1) x += n;
+//         else x -= n;
+//     }
+//     else if (r == 2) {
+//         if (x & 1) x -= 1;
+//         else x += 1;
+//     }
+//     else if (r == 3) {
+//         if (x & 1) x -= (n - 1);
+//         else x += (n - 1);
+//     }
+//     // r == 0 → no change
+
+//     cout << x << "\n";
+// }
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) solve();
+// }
 
 
 // #include <bits/stdc++.h>
