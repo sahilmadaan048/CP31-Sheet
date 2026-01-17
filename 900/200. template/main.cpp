21bf643e22768e3357c717043554bd3b13713b55
Sahil Madaan <madaan.sahil27@gmail.com>
2026-01-17 10:07:45 +0530

    template

---
+++ 900/200. template/main.cpp
@@ -0,0 +1,22 @@
+// https://codeforces.com/problemset/problem/1606/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
