7715b78c596bf8e6406207adf562297c878e3cd1
Sahil Madaan <madaan.sahil27@gmail.com>
2026-02-05 23:32:10 +0530

    Dora and search

---
+++ 1200/8. Dora and Search/main.cpp
@@ -0,0 +1,79 @@
+// Author - sahilmadaan048
+// https://codeforces.com/problemset/problem/1793/C
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+// typedef vector<int> vi;
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    for (int &i: a)
+        cin >> i;
+    int l = 0, r = n - 1;
+    int mn = 1, mx = n;
+    while (l <= r) {
+        if (a[l] == mn) {
+            l++;
+            mn++;
+        } else if (a[l] == mx) {
+            l++;
+            mx--;
+        } else if (a[r] == mn) {
+            r--;
+            mn++;
+        } else if (a[r] == mx) {
+            r--;
+            mx--;
+        } else {
+            break;
+        }
+    }
+    if(l <= r){
+        cout << l + 1 << " " << r + 1 << endl;
+    } else{
+        cout << -1 << endl;
+    }
+}
+
+signed main() {
+    int q = 1;
+    cin >> q;
+    while (q--)
+        solve();
+    return 0;
+}
