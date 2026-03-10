cae6e3a4af6e9c74cde45731a048988c152aa4da
Sahil Madaan <madaan.sahil27@gmail.com>
2026-03-10 20:48:17 +0530

    feat: Grandma Capa Knits a Scarf

---
+++ 1200/20. Grandma Capa Knits a Scarf/main.cpp
@@ -0,0 +1,313 @@
+// https://codeforces.com/problemset/problem/1582/A
+// https://codeforces.com/problemset/problem/1582/B
+// https://codeforces.com/problemset/problem/1582/C
+
+
+// ------------- d
+
+// Author - sahilmadaan048
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+void solve(){
+    int n; cin >> n;
+    vector<int> a(n);
+
+    for(int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+
+    int start = 0;
+
+        if (n % 2 == 1) {
+            if (a[0] + a[1] != 0) {
+                cout << -a[2] << " " << -a[2] << " " << a[0] + a[1] << " ";
+            }
+            else if (a[1] + a[2] != 0) {
+                cout << a[2] + a[1] << " " << -a[0] << " " << -a[0] << " ";
+            }
+            else {
+                cout << -a[1] << " " << a[0] + a[2] << " " << -a[1] << " ";
+            }
+            start = 3;
+        }
+
+        while (start < n) {
+            cout << -a[start + 1] << " " << a[start] << " ";
+            start += 2;
+        }
+        cout << "\n";
+}
+
+int32_t main()
+{
+
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+
+   int T; cin >> T;
+   while (T--)
+   {
+    solve();
+}
+return 0;
+}
+
+
+// ------------- c
+
+// Author - sahilmadaan048
+
+// #include "bits/stdc++.h"
+// #define int long long
+// #define uint unsigned long long
+// #define vi vector<int>
+// #define vvi vector<vi >
+// #define vb vector<bool>
+// #define vvb vector<vb >
+// #define fr(i,n) for(int i=0; i<(n); i++)
+// #define rep(i,a,n) for(int i=(a); i<=(n); i++)
+// #define nl cout<<"\n"
+// #define dbg(var) cout<<#var<<"="<<var<<" "
+// #define all(v) v.begin(),v.end()
+// #define sz(v) (int)(v.size())
+// #define srt(v)  sort(v.begin(),v.end())         // sort 
+// #define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+// #define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+// #define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// // make sure to sort before applying unique // else only consecutive duplicates would be removed 
+// #define bin(x,y)  bitset<y>(x) 
+// using namespace std;
+// int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+// void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+// void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+// void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// // ================================== take ip/op like vector,pairs directly!==================================
+// template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+// template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+// template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+// template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+// template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// // ===================================END Of the input module ==========================================
+
+
+// void solve(){
+//     int n; cin >> n;
+//     string s; cin >> s;
+
+//     int ans = n + 1;
+
+//     for(int i=0; i<26; i++) {
+//         int l = 0, r = n-1, cnt = 0;
+
+//         while(l<=r) {
+//             if(s[l] == s[r]) {
+//                 l++;
+//                 r--;
+//             } else if(s[l] == char('a' + i)) {
+//                 cnt++;
+//                 l++;
+//             } else if(s[r] == char('a' + i)) {
+//                 cnt++;
+//                 r--;
+//             }
+
+//             else {
+//                 cnt = n+1;
+//                 break;
+//             }
+//         }
+//         ans = min(ans, cnt);
+//     }
+
+//     if(ans == n+1) ans = -1;
+//     cout << ans << "\n";
+// }
+
+// int32_t main()
+// {
+
+//  ios_base::sync_with_stdio(false);
+//  cin.tie(NULL);
+
+//     int T; cin >> T;
+//     while (T--)
+//     {
+//         solve();
+//     }
+//     return 0;
+// }
+
+
+// ------------- b
+// Author - sahilmadaan048
+
+// #include "bits/stdc++.h"
+// #define int long long
+// #define uint unsigned long long
+// #define vi vector<int>
+// #define vvi vector<vi >
+// #define vb vector<bool>
+// #define vvb vector<vb >
+// #define fr(i,n) for(int i=0; i<(n); i++)
+// #define rep(i,a,n) for(int i=(a); i<=(n); i++)
+// #define nl cout<<"\n"
+// #define dbg(var) cout<<#var<<"="<<var<<" "
+// #define all(v) v.begin(),v.end()
+// #define sz(v) (int)(v.size())
+// #define srt(v)  sort(v.begin(),v.end())         // sort 
+// #define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+// #define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+// #define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// // make sure to sort before applying unique // else only consecutive duplicates would be removed 
+// #define bin(x,y)  bitset<y>(x) 
+// using namespace std;
+// int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+// void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+// void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+// void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// // ================================== take ip/op like vector,pairs directly!==================================
+// template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+// template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+// template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+// template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+// template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// // ===================================END Of the input module ==========================================
+
+
+// void solve(){
+//     int n; cin >> n;
+//     vector<int> a(n);
+
+//     int ones = 0, zeroes = 0;
+
+//     for(int i=0; i<n; i++) {
+//         cin >> a[i];
+//         if(a[i] == 1) ones++;
+//         else if(a[i] == 0) zeroes++;
+//     }
+
+//     if(ones == 0) {
+//         cout << 0 << "\n";
+//         return;
+//     }
+
+//     int ans = ones * (1LL << zeroes);
+//     cout << ans << "\n";
+
+// }
+
+// int32_t main()
+// {
+
+//  ios_base::sync_with_stdio(false);
+//  cin.tie(NULL);
+
+//     int T ; cin >> T;
+//     while (T--)
+//     {
+//         solve();
+//     }
+//     return 0;
+// }
+
+
+
+// ------------- a
+
+
+// Author - sahilmadaan048
+
+// #include "bits/stdc++.h"
+// #define int long long
+// #define uint unsigned long long
+// #define vi vector<int>
+// #define vvi vector<vi >
+// #define vb vector<bool>
+// #define vvb vector<vb >
+// #define fr(i,n) for(int i=0; i<(n); i++)
+// #define rep(i,a,n) for(int i=(a); i<=(n); i++)
+// #define nl cout<<"\n"
+// #define dbg(var) cout<<#var<<"="<<var<<" "
+// #define all(v) v.begin(),v.end()
+// #define sz(v) (int)(v.size())
+// #define srt(v)  sort(v.begin(),v.end())         // sort 
+// #define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+// #define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+// #define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// // make sure to sort before applying unique // else only consecutive duplicates would be removed 
+// #define bin(x,y)  bitset<y>(x) 
+// using namespace std;
+// int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+// void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+// void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+// void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// // ================================== take ip/op like vector,pairs directly!==================================
+// template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+// template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+// template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+// template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+// template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// // ===================================END Of the input module ==========================================
+
+
+// void solve(){
+//     int a, b, c; cin >> a >> b >> c;
+//     int sum = a + b*2+ c*3;
+
+//     cout << (a + c) % 2 << "\n";
+// }
+
+
+// int32_t main()
+// {
+
+//  ios_base::sync_with_stdio(false);
+//  cin.tie(NULL);
+
+//  int T; cin >> T;
+//  while (T--)
+//  {
+//     solve();
+// }
+// return 0;
+// }
